30615cb1990408bd8e81245426b2da6702d6532f
helwyza <helwyza@gmail.com>
2026-04-04 13:21:04 +0700

    x

--- saas/internal/platform/database/postgres.go
+++ saas/internal/platform/database/postgres.go
@@ -2,15 +2,21 @@ package database
 
 import (
 	"fmt"
+	"os" // Tambahkan import os
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
 func NewPostgres(host, port, user, password, dbname string) (*sqlx.DB, error) {
-	// Tambahkan sslmode=require di sini
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
-		host, port, user, password, dbname)
+	// Ambil sslmode dari env, jika kosong default ke require (aman untuk RDS)
+	sslMode := os.Getenv("DB_SSLMODE")
+	if sslMode == "" {
+		sslMode = "require"
+	}
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslMode)
 
 	db, err := sqlx.Open("postgres", dsn)
 	if err != nil {
